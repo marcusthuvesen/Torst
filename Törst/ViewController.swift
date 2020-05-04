84e8a8ac5d021c0cfdf5cfe87c8b7772629e62b7
Marcus Thuvesen <Marcus.Thuvesen@cybercom.com>
2020-05-04 16:54:58 +0200

    Initial Commit

---
+++ Törst/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Törst
+//
+//  Created by Marcus Thuvesen on 2020-05-04.
+//  Copyright © 2020 Marcus Thuvesen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
