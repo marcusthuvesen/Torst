39c6c27358cea278c784666b3308a0f3eace47be
Marcus Thuvesen <Marcus.Thuvesen@cybercom.com>
2020-05-27 20:42:40 +0200

    rulestesxt

--- Törst/Model/ProvideGameRules.swift
+++ Törst/Model/ProvideGameRules.swift
@@ -13,28 +13,29 @@ struct ProvideGameRules {
     
     
     func jagHarAldrigRules() -> String {
-        
-        return "jagHarAldrig"
+        let jagHarAldrigRules = "Alla deltagare ska placera sig så att ni ser varandra.\n\nSpelaren med telefonen läser upp påståendena som startar med \("Jag har aldrig...") alla som gjort det som läses upp ska dricka, om endast en person dricker måste man förklara/beskriva händelsen."
+        return jagHarAldrigRules
     }
     
     func peklekenRules() -> String {
+        let peklekenRules = "Alla deltagare skall placera sig så att ni ser varandra.\n\nSpelaren med telefonen läser upp påståenden. Sedan ska alla deltagare enskilt välja den person de tycker passar in bäst på påståendet och peka på denna. Den person som fått flest pek dricker."
         
-        return "pekleken"
+        return peklekenRules
     }
     
     func ryggMotRyggRules() -> String{
-        
-        return "RyggMotRygg"
+        let ryggMotRyggRules = "Två spelare placerar stolar med ryggarna mot varandra och sätter sig så att de inte kan se den andre personen.\n\nEn annan person med telefonen läser upp påståenden och spelarna på stolen ska peka på den person som de tycker att det stämmer bäst in på. Om båda pekar på samma ska resten av deltagarna dricka, annars dricker de två spelarna."
+        return ryggMotRyggRules
     }
     
     func utmaningarRules() -> String{
-       
-        return "utmaningar"
+        let utmaningarRules = "Alla deltagare skall placera sig så att ni ser varandra.\n\nSpelaren med telefonen läser upp utmaningar till den första personen till vänster och fortsätter sedan runt i gruppen. Personen som får utmaningen måste göra denna eller dricka 5 klunkar."
+        return utmaningarRules
     }
     
     func mixRules() -> String {
-        
-        return "mix"
+        let mixRules = "mixregler"
+        return mixRules
     }
     
     
